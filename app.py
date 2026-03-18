e5b5f5795d693488e4b696a808a655364f21011b
Manu <manuelgm6@hotmail.com>
2026-03-18 23:54:27 +0100

    Creando Futuros archivos y gitignore

--- app.py
+++ app.py
@@ -3,3 +3,11 @@ from flask import Flask, render_template, request, jsonify
 
 
 app = Flask(__name__)
+
+#el approute es para ejecutar o redirigir
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-class Config:
-    SPEECH_KEY = os.getenv("SPEECH_KEY")
-    SPEECH_REGION = os.getenv("SPEECH_REGION")#hola
